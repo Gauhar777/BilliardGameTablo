13aec5bbd3de280b9bec1d84097626541e3ef4bd
Gauhar777 <gauhar777@list.ru>
2018-07-17 10:03:09 +0600

    version0.2

--- src/main/java/kz/ivc/games/dto/ResultGameDTO.java
+++ src/main/java/kz/ivc/games/dto/ResultGameDTO.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 @Data
 public class ResultGameDTO implements Serializable {
     private long idGamer;
+    private long idCompetition;
+    private long id;
     private String nick;
     private long point1;
     private long point2;
