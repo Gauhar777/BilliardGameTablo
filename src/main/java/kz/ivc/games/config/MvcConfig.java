d34658b51c71fdfa1b3ce9a14264dae3a0976b8c
Gauhar777 <gauhar777@list.ru>
2018-08-15 21:18:18 +0600

    LogIN+pointAddwhenbuttonCheked

---
+++ src/main/java/kz/ivc/games/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package kz.ivc.games.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+    abstract class MvcConfig implements WebMvcConfigurer {
+   /* public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/main2").setViewName("listCompetition");
+        registry.addViewController("/login").setViewName("login");
+
+    }*/
+}
---
+++ src/main/java/kz/ivc/games/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package kz.ivc.games.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+   @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("i")
+                        .password("123")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+ /*@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+     auth.inMemoryAuthentication().withUser("user").password("123").roles("user");
+ }*/
+}
