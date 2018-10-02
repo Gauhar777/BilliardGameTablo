4c8a64d47711823b0b839e14487b9ee84385f8d8
Gauhar777 <gauhar777@list.ru>
2018-10-02 18:28:07 +0600

    before upload file and java script

---
+++ src/main/java/kz/ivc/games/config/PhotoConfig.java
@@ -0,0 +1,12 @@
+package kz.ivc.games.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class PhotoConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/upload").setViewName("photoupload");
+    }
+}
