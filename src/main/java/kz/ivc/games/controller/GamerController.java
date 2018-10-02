53049c32ce6de75143aa500245d1175362048402
Gauhar777 <gauhar777@list.ru>
2018-10-02 18:29:07 +0600

    before upload file and java script

--- src/main/java/kz/ivc/games/controller/GamerController.java
+++ src/main/java/kz/ivc/games/controller/GamerController.java
@@ -2,20 +2,18 @@ package kz.ivc.games.controller;
 
 import kz.ivc.games.dto.GamerForm;
 import kz.ivc.games.dto.GamerOfCompetition;
+import kz.ivc.games.dto.BestGamersDTO;
+import kz.ivc.games.dto.ResultDTO;
 import kz.ivc.games.entity.*;
 import kz.ivc.games.repo.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 @Controller
@@ -142,27 +140,25 @@ public class GamerController {
 
 
     @RequestMapping(value = {"{idC}/addGamers"}, method = RequestMethod.POST)
-    public String gamersSave(@PathVariable Long idC,@ModelAttribute("gamersForm") GamerForm gamersForm) {
-        Gamer gamer = new Gamer();
-        String FIO = gamersForm.getFIO();
-        String nick = gamersForm.getNick();
-        if ((FIO != null && nick != null) && (FIO.length() > 0 && nick.length() > 0)) {
-            gamer.setFIO(FIO);
-            gamer.setNick(nick);
-            this.gamerRepo.save(gamer);
-            return "redirect:/competition/{idC}/addGamers";
-        } else {
-            return "editGamer";
-        }
+    public String gamersSave( @ModelAttribute("newGamer") Gamer newGamer,@PathVariable Long idC, GamerForm gamersForm,ModelMap model) {
+        model.addAttribute("name", newGamer.getFIO());
+        model.addAttribute("id", newGamer.getNick());
+        this.gamerRepo.save(newGamer);
+        return "redirect:/competition/{idC}/addGamers";
     }
 
 
     //***********************************************Delete********************************************
-    @RequestMapping(value = "/{idC}/deleteGamer/{id}", method = RequestMethod.GET)
-    public String deleteGamer(@PathVariable Long id,@PathVariable Long idC) {
-        Gamer gamer = this.gamerRepo.getOne(id);
-        this.gamerRepo.delete(gamer);
-
+    @RequestMapping(value = "/{idC}/deleteGamer/{id}", method = {RequestMethod.GET,RequestMethod.POST})
+    public String deleteGamer(@ModelAttribute("model") ModelMap model, @PathVariable Long id,@PathVariable Long idC) {
+        try {
+            Gamer gamer = this.gamerRepo.getOne(id);
+            this.gamerRepo.delete(gamer);
+        }catch(Exception ex){
+            String mess="This gamer plays in other game!";
+            model.put("mess",mess);
+            return "messages";
+        }
         return "redirect:/competition/{idC}/addGamers";
     }
     //***********************************************Dezhurny********************************************
@@ -186,4 +182,66 @@ public class GamerController {
         return "redirect:/competition/{idC}/addGamers";
     }
 
+    //***********************************************scoreOfGamers********************************************
+
+    @RequestMapping(value = "/listGamer", method = RequestMethod.GET)
+    public String listGamer(@ModelAttribute("model") ModelMap model) {
+        model.addAttribute("resource",resource);
+
+        List<BestGamersDTO> bestGamerslist=new ArrayList<BestGamersDTO>();
+        long agrPoint1;  // salgan sharlar sany
+        long agrPoint2;  // jibergen sharlar sany
+        long def;
+
+        List<Gamer> gamerList=this.gamerRepo.findAll();
+        for(Gamer gamer:gamerList) {
+            agrPoint2 = 0;  // jibergen sharlar sany
+            agrPoint1 = 0;  // salgan sharlar sany
+            int agrBall;
+            agrBall=0;
+            LOG.info("----"+gamer);
+            BestGamersDTO bestGamer = new BestGamersDTO();
+
+            bestGamer.setId(gamer.getId());
+            bestGamer.setNick(gamer.getNick());
+            bestGamer.setFIO(gamer.getFIO());
+
+            List<Partner> partnerList=this.partnerRepo.findByIdGamer(gamer.getId());
+            for(Partner partner1:partnerList) {
+                List<Game> gameList=this.gameRepo.findByIdPartner1(partner1.getId());
+                List<Game> gameMirrorList=this.gameRepo.findByIdPartner2(partner1.getId());
+
+                if(gameList!=null){
+                    for (Game game:gameList){
+                        if(game.getPoint1()>game.getPoint2()){
+                            ++agrBall;
+                        }
+                        agrPoint1=agrPoint1+game.getPoint1();
+                        agrPoint2=agrPoint2+game.getPoint2();
+                    }
+                }
+                if(gameMirrorList!=null){
+                    for (Game gameMirror:gameMirrorList){
+                        if(gameMirror.getPoint2()>gameMirror.getPoint1()){
+                            ++agrBall;
+                        }
+                        agrPoint1=agrPoint1+gameMirror.getPoint2();
+                        agrPoint2=agrPoint2+gameMirror.getPoint1();
+                    }
+                }
+
+            }
+            def=agrPoint1-agrPoint2;
+            bestGamer.setAgrBall(agrBall);
+            bestGamer.setDeference(def);
+
+            bestGamerslist.add(bestGamer);
+        }
+        bestGamerslist.sort(Comparator.comparing(BestGamersDTO::getAgrBall)
+                .reversed()
+                .thenComparing(Comparator.comparing(BestGamersDTO::getDeference)
+                .reversed()) );
+        model.addAttribute("gamer",bestGamerslist);
+        return"listGamer";
+    }
 }
